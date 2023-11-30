7551dc0400bbf006774fd0de8b099b9b57132f44
Alex <a.blanc100@laposte.net>
2023-11-30 18:51:26 +0100

    Fix message sender

--- src/main/java/lu/sfeir/commerce/stocks/events/KafkaProducerConfig.java
+++ src/main/java/lu/sfeir/commerce/stocks/events/KafkaProducerConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -30,7 +31,7 @@ public class KafkaProducerConfig {
           StringSerializer.class);
         configProps.put(
           ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, 
-          StockUpdatedSerializer.class);
+          JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
